feea8ae7ba1e4f67216b68ae745340adc8faefd1
Svante M Haggblom <svanteha@svm-19.cs.helsinki.fi>
2013-01-28 14:25:47 +0200

    runkuja kehitetty

--- deittipalvelu/avusteet/yla.php
+++ deittipalvelu/avusteet/yla.php
@@ -1,3 +1,7 @@
+<?php
+  $user = user_info();
+?>
+
 <!DOCTYPE html>
 <html>
 <head>
@@ -5,4 +9,23 @@
 <body>
 <h1>Deittipalvelu</h1>
 
+<div>
+
+<?php if($user){ ?>
+
+<p>Olet kirjautunut käyttäjänä <?php echo $user->user_name?></p>
+
+<?php if($user->admin){ ?>
+
+<p>Olet admin</p>
+
+<?php } ?>
+<?php } else { ?>
+<p><a href="sign_in.php">Kirjaudu sisään</a></p>
+<?php } ?>
+</div>
+<div>
+<a href="index.php">Etusivulle</a>
+</div>
+<br></br>
 
