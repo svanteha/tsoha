02bdfb711bcdbda0a0a8742a65287f10d7880b0c
Svante M Haggblom <svanteha@svm-19.cs.helsinki.fi>
2013-01-28 14:14:01 +0200

    lisatty tavaraa

--- deittipalvelu/index.php
+++ deittipalvelu/index.php
@@ -3,6 +3,10 @@
  require('avusteet/kanta.php');
  $user = user_info();
 
+ $user_query = create_connection()->prepare("SELECT * FROM Users");
+ $user_query->execute();
+
+
 ?>
 
 <p>Tervetuloa Deittipalveluun! Täältä voit löytää itsellesi kumppanin!</p>
@@ -10,7 +14,11 @@
 
 <h2>Käyttäjälista</h2>
 
-
+<ul>
+<?php while($us = $user_query->fetchObject()) { ?>
+<li><a><?php echo $us->user_name; ?></a></li>
+<?php } ?>
+</ul>
 
 
 
