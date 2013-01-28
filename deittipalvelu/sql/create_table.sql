1b8b006d8a5c19126efa3eeab2e90f4e7be38b19
Svante M Haggblom <svanteha@svm-19.cs.helsinki.fi>
2013-01-28 11:53:17 +0200

    tauluja kehitetty

--- deittipalvelu/sql/create_table.sql
+++ deittipalvelu/sql/create_table.sql
@@ -1,7 +1,47 @@
-CREATE TABLE kayttajat(
-id serial PRIMARY KEY,
-tunnus varchar UNIQUE CHECK (char_length(tunnus) > 2),
-salasana varchar CHECK (char_length(salasana) > 5),
-banned boolean DEFAULT false,
-admin boolean DEFAULT false
+CREATE TABLE users(
+user_id serial PRIMARY KEY,
+user_name VARCHAR(255) UNIQUE CHECK (char_length(user_name) > 2),
+password VARCHAR(255) CHECK (char_length(password) > 5),
+banned BOOLEAN DEFAULT false,
+admin BOOLEAN DEFAULT false,
+age INT CHECK (age>18),
+gender VARCHAR(255),
+country VARCHAR(255),
+city VARCHAR(255),
+description VARCHAR(255),
+first_name VARCHAR(255),
+last_name VARCHAR(255),
+phone_number BIGINT(10),
+email VARCHAR(255)
+);
+
+CREATE TABLE sites(
+site_id serial PRIMARY KEY,
+user_id  INT FOREIGN KEY,
+secret BOOLEAN DEFAULT true,
+date_created DATETIME()
+);
+
+CREATE TABLE messages(
+message_id serial PRIMARY KEY,
+from_user_id INT FOREIGN KEY,
+to_user_id INT FOREIGN KEY,
+date_sent TIMESTAMP(),
+subject VARCHAR(255),
+message TEXT
+);
+
+CREATE TABLE contacts(
+contact_id serial PRIMARY KEY,
+user_id INT FOREIGN KEY,
+role_code INT FOREIGN KEY,
+date_contact_from DATE(),
+contact_email VARCHAR(255),
+contact_name VARCHAR(255),
+contact_phone BIGINT()
+);
+
+CREATE TABLE role(
+role_code serial PRIMARY KEY,
+description VARCHAR(255)
 );
